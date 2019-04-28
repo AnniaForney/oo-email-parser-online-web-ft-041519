7ccb48445ad9b10c5b4e64778bda5f3528617091
Annia Forney <anniaforney@gmail.com>
2019-04-28 21:20:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,14 +3,14 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-  attr_accessor :email 
+  attr_accessor :email
 
   def initialize(emails)
-    @emails = emails 
+    @emails = emails
 end
 
-def parse 
-  
-end 
+def parse
 
-end 
+end
+
+end
